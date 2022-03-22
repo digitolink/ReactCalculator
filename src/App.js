e875c18709c3b06f31f957713aa7751b28589b8d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-22 10:06:41 +0100

    ej calculadora hecha

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
   const [ firstNumber, setFirstNumber ] = useState(null);
+  const [ secondNumber, setSecondNumber ] = useState(null);
+  const [ result, setResult ] = useState(null);
+  const  memory = useRef(null);
 
   function changeFirstNumberHandler (event) {
     setFirstNumber(event.target.value);
   }
 
+  function changeSecondNumberHandler (event) {
+    setSecondNumber(event.target.value);
+  }
+
+  function addHandler (event){
+    setResult(parseFloat(firstNumber)+parseFloat(secondNumber));
+  }
+  function substractionHandler (event){
+    setResult(parseFloat(firstNumber)-parseFloat(secondNumber));
+  }
+  function multiplyHandler (event){
+    setResult(parseFloat(firstNumber)*parseFloat(secondNumber));
+  }
+  function divideHandler (event){
+    setResult(parseFloat(firstNumber)/parseFloat(secondNumber));
+  }
+
+  function deleteHandler (event){
+    setResult(null);
+    setSecondNumber("");
+    setFirstNumber("");
+  }
+
+  function memoryHandler(event){
+    memory.current=result;
+    console.log(memory);
+  }
+
+  function copyHandler(event){
+    setFirstNumber(memory.current);
+  }
+  
   useEffect(
     ()=>{
       console.log("firstNumber state:", firstNumber);
+      console.log("secondNumber state:", secondNumber);
     }
   )
 
@@ -18,6 +54,17 @@ function App() {
     <>
       <h1>Calculadora</h1>
       <input type="text" value={firstNumber} onChange={changeFirstNumberHandler}/>
+      <br></br>
+      <input type="text" value={secondNumber} onChange={changeSecondNumberHandler}/>
+      <p>{result}</p>
+      <input type="button" value={"+"} onClick={addHandler}/>
+      <input type="button" value={"-"} onClick={substractionHandler}/>
+      <input type="button" value={"*"} onClick={multiplyHandler}/>
+      <input type="button" value={"/"} onClick={divideHandler}/>
+      <input type="button" value={"C"} onClick={deleteHandler}/>
+      <input type="button" value={"M+"} onClick={memoryHandler}/>
+      <input type="button" value={"MR"} onClick={copyHandler}/>
+      
     </>
   );
 }
